4b8b71e946849b740d421972883e044523e1cb06
yinyue <yinyue@riz-tech.com>
2025-04-25 15:05:11 +0800

    fix bug in compile bat

--- build_pico.bat
+++ build_pico.bat
@@ -1,6 +1,19 @@
 @echo off
 echo  start build...
 
+rem 设置Pico SDK路径
+set PICO_SDK_PATH=C:\Program Files\Raspberry Pi\Pico SDK v1.5.1\pico-sdk
+echo  set PICO_SDK_PATH to %PICO_SDK_PATH%
+
+rem 复制pico_sdk_import.cmake文件(如果存在)
+if exist "%PICO_SDK_PATH%\external\pico_sdk_import.cmake" (
+    echo  copying pico_sdk_import.cmake from SDK external directory...
+    copy "%PICO_SDK_PATH%\external\pico_sdk_import.cmake" .
+) else if exist "%PICO_SDK_PATH%\pico_sdk_import.cmake" (
+    echo  copying pico_sdk_import.cmake from SDK root...
+    copy "%PICO_SDK_PATH%\pico_sdk_import.cmake" .
+)
+
 if exist build (
     echo  clean old build files...
     rd /s /q build
@@ -19,4 +32,4 @@ if %ERRORLEVEL% NEQ 0 (
 
 echo  build success!
 echo UF2 file has been generated in the build directory
-pause 
+cd ..
